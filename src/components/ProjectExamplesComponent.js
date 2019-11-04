f9183c1966603f59389b515e3c0b9dd663da859f
ReedS <ReedStock1992@gmail.com>
2019-11-03 18:14:05 -0800

    - ProjectExampleComponent    - Added image templates in column next to project carousel

--- src/components/ProjectExamplesComponent.js
+++ src/components/ProjectExamplesComponent.js
@@ -1,49 +1,74 @@
 import React, { Component } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Grid, GridRow, GridColumn } from "semantic-ui-react";
+import { Grid, GridRow, GridColumn, Image } from "semantic-ui-react";
 
 export default class ProfileComponent extends Component {
     render() {
         return (
-            <GridRow
-                centered
-                style={{
-                    backgroundColor: "red",
-                    marginTop: -20,
-                    paddingLeft: 10,
-                    paddingRight: 10
-                }}
-            >
-                <GridColumn>
-                    <Carousel
-                        selectedItem={1}
-                        showThumbs={false}
-                        showStatus={false}
-                        infiniteLoop
-                        useKeyboardArrows
-                        autoPlay
-                        emulateTouch
-                        transitionTime={1500}
-                        animation="slide left"
-                        showNextPrev={false}
-                        showIndicators={true}
-                    >
-                        <div>
-                            <img src={require("/resources/project1.png")} />
-                            <p className="legend">Legend 1</p>
-                        </div>
-                        <div>
-                            <img src={require("/resources/project2.png")} />
-                            <p className="legend">Legend 2</p>
-                        </div>
-                        <div>
-                            <img src={require("/resources/project3.png")} />
-                            <p className="legend">Legend 3</p>
-                        </div>
-                    </Carousel>
-                </GridColumn>
-            </GridRow>
+            <Grid>
+                <GridRow
+                    columns={3}
+                    style={{ paddingLeft: "2%", paddingRight: "2%" }}
+                >
+                    <GridColumn width={10}>
+                        <Carousel
+                            selectedItem={1}
+                            showThumbs={false}
+                            showStatus={false}
+                            infiniteLoop
+                            useKeyboardArrows
+                            autoPlay
+                            emulateTouch
+                            transitionTime={1500}
+                            animation="slide left"
+                            showNextPrev={false}
+                            showIndicators={true}
+                        >
+                            <div>
+                                <img src={require("/resources/project1.png")} />
+                                <p className="legend">Legend 1</p>
+                            </div>
+                            <div>
+                                <img src={require("/resources/project2.png")} />
+                                <p className="legend">Legend 2</p>
+                            </div>
+                            <div>
+                                <img src={require("/resources/project3.png")} />
+                                <p className="legend">Legend 3</p>
+                            </div>
+                        </Carousel>
+                    </GridColumn>
+                    <GridColumn width={3}>
+                        <Image
+                            src={require("/resources/placeholder.jpg")}
+                            centered
+                        />
+                        <Image
+                            src={require("/resources/placeholder.jpg")}
+                            centered
+                        />
+                        <Image
+                            src={require("/resources/placeholder.jpg")}
+                            centered
+                        />
+                    </GridColumn>
+                    <GridColumn width={3}>
+                        <Image
+                            src={require("/resources/placeholder.jpg")}
+                            centered
+                        />
+                        <Image
+                            src={require("/resources/placeholder.jpg")}
+                            centered
+                        />
+                        <Image
+                            src={require("/resources/placeholder.jpg")}
+                            centered
+                        />
+                    </GridColumn>
+                </GridRow>
+            </Grid>
         );
     }
 }
