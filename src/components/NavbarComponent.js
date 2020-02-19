3e31cdfaf4402ec3109d94f0e4167fd53b3f4e2d
ReedS <ReedStock1992@gmail.com>
2020-02-18 21:10:07 -0800

    - bug fixed missing extra segment tag

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -30,46 +30,44 @@ export default class NavbarComponent extends Component {
                 <Grid centered style={{ backgroundColor: "black" }}>
                     <GridRow>
                         <GridColumn>
-                                <Menu
-                                    stackable
-                                    inverted
-                                    pointing
-                                    secondary
-                                    widths={4}
-                                >
-                                    <Menu.Item
-                                        href="#home"
-                                        name="home"
-                                        active={
-                                            this.props.activeMenuItem === "home"
-                                        }
-                                        onClick={this.handleItemClick}
+                            <Menu
+                                stackable
+                                inverted
+                                pointing
+                                secondary
+                                widths={4}
+                            >
+                                <Menu.Item
+                                    href="#home"
+                                    name="home"
+                                    active={
+                                        this.props.activeMenuItem === "home"
+                                    }
+                                    onClick={this.handleItemClick}
+                                />
+                                <Menu.Item
+                                    href="#about"
+                                    name="about"
+                                    active={
+                                        this.props.activeMenuItem === "about"
+                                    }
+                                    onClick={this.handleItemClick}
+                                />
+                                <Menu.Item
+                                    href="#contact-me"
+                                    name="contact-me"
+                                    active={
+                                        this.props.activeMenuItem ===
+                                        "contact-me"
+                                    }
+                                    onClick={this.handleItemClick}
+                                />
+                                <Menu.Item position="right">
+                                    <SocialMediaComponent
+                                        openModal={this.props.openModal}
                                     />
-                                    <Menu.Item
-                                        href="#about"
-                                        name="about"
-                                        active={
-                                            this.props.activeMenuItem ===
-                                            "about"
-                                        }
-                                        onClick={this.handleItemClick}
-                                    />
-                                    <Menu.Item
-                                        href="#contact-me"
-                                        name="contact-me"
-                                        active={
-                                            this.props.activeMenuItem ===
-                                            "contact-me"
-                                        }
-                                        onClick={this.handleItemClick}
-                                    />
-                                    <Menu.Item position="right">
-                                        <SocialMediaComponent
-                                            openModal={this.props.openModal}
-                                        />
-                                    </Menu.Item>
-                                </Menu>
-                            </Segment>
+                                </Menu.Item>
+                            </Menu>
                         </GridColumn>
                     </GridRow>
                 </Grid>
